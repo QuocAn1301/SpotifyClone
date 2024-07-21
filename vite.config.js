01c180c5fda6c8e7888aa05c2e658c794ed83525
QuocAn1301 <137316838+QuocAn1301@users.noreply.github.com>
2024-07-22 02:07:34 +0700

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Spotify-clone/",
+  base: "/SpotifyClone/",
   plugins: [react()],
 
 })
